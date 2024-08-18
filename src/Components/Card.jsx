93837461f7d73cc49986b34e0ef89c37e67ce8d9
alamshuvo <iftakharalamshuvo11@gmail.com>
2024-08-17 22:33:55 -0700

    some code added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 
 const Card = ({ product }) => {
-    const localDate = new Date(product.creationDate).toLocaleString();
+    const localDate = new Date(product.createdAt).toLocaleString();
   
     return (
-      <div className="max-w-lg p-4 border-2 rounded border-blue-800 shadow-md bg-gray-100 dark:text-gray-800">
+      <div className="max-w-lg p-4 rounded-lg  border-2  border-blue-800 shadow-md ">
         <div className="flex text-blue-800 mb-1 justify-between  border-bottom">
           <p>{product.category}</p>
           <p>{product.brandName}</p>
         </div>
         <div className="space-y-1 ">
-          <div className="space-y-2">
+          <div className="space-y-2 border-2 p-2 rounded-lg">
             <img
               src={product.productImage}
               alt={product.productName}
               className="block object-cover object-center w-full rounded-md h-72 "
             />
-            <div className="flex items-center justify-between text-sm">
+            <div className="flex items-center justify-between text-sm ">
               <p>Price: ${product.price}</p>
-              {/* <Rating
-                style={{ maxWidth: 100 }}
-                value={product.ratings}
-                readOnly
-              /> */}
+             
             </div>
           </div>
-          <div className="">
+          <div className="border-2 p-2 ">
             <h3 className="text-xl font-semibold dark:text-violet-600">
               {product.productName}
             </h3>
